006d98ccbee7b3a1cee67bb556347eae94fa2bee
shadreza <shadreza100@gmail.com>
2022-07-25 23:13:45 +0600

    This is the initial commit with a helloworld view and controller

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            // return "This is a Index page!";
+        }
+
+        public string Hello()
+        {
+            return "Who is there?";
+        }
+    }
+}
